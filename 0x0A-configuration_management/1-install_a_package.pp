bcc764b120fe2e432e79de44b634ca71e5162807
mucyochris47 <mucyochris250@gmail.com>
2025-01-10 08:40:22 -0800

    Dtask 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-# Install specific version of flask
-
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+# Install the puppet-lint package
+package { 'puppet-lint':
+  ensure   => '2.5.0',
+  provider => 'gem',
 }
