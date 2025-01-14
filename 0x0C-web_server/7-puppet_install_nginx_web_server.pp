658e31517f5d340e7503aa1f702a32847b4d5cc8
mucyochris47 <mucyochris250@gmail.com>
2025-01-14 15:23:26 -0800

    task 5

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,20 +1,22 @@
 # automating configuration with puppet
 
+# Install Nginx with puppet
 package { 'nginx':
   ensure => installed,
 }
 
 file_line { 'install':
   ensure => 'present',
-  path   =? '/etc/nginx/sites-available/default',
+  path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
-  line   =>'rewrite ^/redirect_me https://www.youtube.com/@christechcoder permanent;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
-file {'/var/www/html/index.html':
-  content => Hello World!;,
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
 }
 
 service { 'nginx':
   ensure  => running,
-  require => package['nginx'],
+  require => Package['nginx'],
+}
